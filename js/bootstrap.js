1b7d3694316bf9d0d1d5136cf28b26ac4475311e
IlyaR <hoiaiski@mail.ru>
2024-07-03 14:04:17 +0300

    Версия 1.0

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,34 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const image = document.querySelector('.animated-image');
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          image.classList.add('animated-image-visible');
-        }
-      });
-    });
-
-    observer.observe(image);
-  });
-
   document.addEventListener("DOMContentLoaded", function() {
     const image = document.querySelector('.animated-image');
-    const texts = document.querySelectorAll('.animated-text'); // Выбираем все элементы с классом ".animated-text"
-
+    const texts = document.querySelectorAll('.animated-text'); 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 if (entry.target.classList.contains('animated-image')) {
                     entry.target.classList.add('animated-image-visible');
                 } else {
-                    entry.target.classList.add('animated-text-visible'); // Добавляем класс для анимации текста
+                    entry.target.classList.add('animated-text-visible'); 
                 }
-                observer.unobserve(entry.target); // Останавливаем наблюдение для элемента
+                observer.unobserve(entry.target); 
             }
         });
-    }, { threshold: 0.1 }); // Пороговое значение для запуска анимации
+    }, { threshold: 0.1 }); 
 
     observer.observe(image);
-    texts.forEach(text => observer.observe(text)); // Начинаем наблюдение для каждого текстового элемента
+    texts.forEach(text => observer.observe(text)); 
 });
